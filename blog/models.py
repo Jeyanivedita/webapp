688a70ef7608103a985c4e78f68d80b9cb5ddb1b
Jeyanivedita <nivedita187@gmail.com>
2020-12-14 23:17:50 +0530

    first commit

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 import datetime
+
 class Post(models.Model):
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=1000)
-    images = models.ImageField(upload_to='blog_images', null=True, default='placeholder.jpg')
+    images = models.ImageField(upload_to='blog_images', blank=True)
     date_posted = models.DateTimeField(default=datetime.datetime.now())
     author = models.CharField(max_length=40)
 
